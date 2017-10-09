2b5960155f59f0f3f574b9c8f38808f09186ab0c
Tim Repke <tim@repke.eu>
2017-10-09 10:22:13 +0200

    enno understands multiple configs and text

--- enno/__init__.py
+++ enno/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import json
+import argparse
 
+parser = argparse.ArgumentParser(description='Enno - Your friendly Text Annotator')
+parser.add_argument('config', nargs='?', default='config-enron.json')
+parser.add_argument('--config', dest='config', default=None)
+args = parser.parse_args()
 
-f = open('config.json', 'r')
+f = open(args.config, 'r')
 config = json.loads(f.read())
 f.close()
 
---
+++ enno/wrappers/text.py
@@ -0,0 +1,90 @@
+import os
+import json
+import sys
+from email import parser as ep
+from enno.utils.annotation import Annotation
+
+
+def __has_annotation(path):
+    return os.path.isfile(path + '.ann')
+
+
+def get_listing(options):
+    path = options['path']
+    ret = {
+        'folders': {},
+        'docs': [],
+        'flat': []
+    }
+    for p, d, f in os.walk(path):
+        tmp = ret
+        p = p[len(path):]
+        for pp in p.split('/'):
+            if len(pp) == 0:
+                continue
+            if pp not in tmp['folders']:
+                tmp['folders'][pp] = {'folders': {}, 'docs': []}
+            tmp = tmp['folders'][pp]
+
+        for ff in f:
+            if ff.endswith('.txt'):
+                sample = {
+                    'name': ff,
+                    'has_annotation': __has_annotation(os.path.join(options['path'], p, ff)),
+                    'sample': p + '/' + ff
+                }
+                tmp['docs'].append(sample)
+                ret['flat'].append(sample)
+        tmp['docs'] = sorted(tmp['docs'], key=lambda k: k['name'])
+
+    ret['flat'] = sorted(ret['flat'], key=lambda k: k['name'])
+    return ret
+
+
+def get_sample(sample, options):
+    path = os.path.join(options['path'], sample)
+    if __has_annotation(path):
+        anno = Annotation.from_file(path + '.ann')
+        if len(anno.id) == 0:  # legacy fix (some might not have an ID)
+            anno.id = sample
+        return anno
+
+    f = open(path, 'r')
+    text = f.read()
+    f.close()
+
+    anno = Annotation()
+    anno.id = sample
+    anno.meta = {}
+    anno.wrapper = 'text'
+    anno.text = text
+
+    return anno
+
+
+def get_last_sample_id(options):
+    path = os.path.join(options['path'], '.last')
+    f = open(path, 'r')
+    sample = f.read()
+    f.close()
+    if os.path.isfile(os.path.join(options['path'], sample)):
+        return sample
+    return get_listing(options)['flat'][0]
+
+
+def set_last_sample(sample, options):
+    with open(os.path.join(options['path'], '.last'), 'w') as f:
+        f.write(sample)
+
+
+def save_sample(sample, payload, options):
+    # TODO make this more fancy using the Annotation class
+    path = os.path.join(options['path'], sample) + '.ann'
+    with open(path, 'w') as f:
+        json.dump(payload, f)
+
+
+def save_annotation(sample, anno, options):
+    path = os.path.join(options['path'], sample) + '.ann'
+    with open(path, 'w') as f:
+        f.write(str(anno))
